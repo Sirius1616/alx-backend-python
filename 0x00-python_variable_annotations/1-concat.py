ece7dd571bb7696dc1f2983b2922db8e28519818
Sirius1616 <siriusa1.615@gmail.com>
2024-10-11 15:26:03 -0700

    lockbox

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""string concatenator function
+"""
+
+
+def concat(str1 : str, str2 : str) -> str:
+    """
+    A function for concatenating strings
+    """
+    return str1 + str2
